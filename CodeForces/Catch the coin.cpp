150c744a6c58c8c06fdc85c38cba1cfa9247d22b
Fahim <fahimimam786@gmail.com>
2025-10-17 22:38:23 +0600

    More coding problems added

---
+++ CodeForces/Catch the coin.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int n;
+cin >> n;
+
+while (n--)
+{
+    int a,b;
+
+    cin >> a >> b;
+
+    if(b >= -1)
+        cout << "YES" << endl;
+
+    else
+        cout << "NO" << endl;
+}
+}
