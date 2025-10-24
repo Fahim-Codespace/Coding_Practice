ea2776e48d0cbec54ba752245f321bca718c163c
Fahim <fahimimam786@gmail.com>
2025-10-24 22:44:11 +0600

    Another coding problem added

---
+++ CodeForces/Dalton the teacher.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >>t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int count = 0;
+        vector<int> arr(n);
+        for(int i = 0; i<n; i++)
+        {
+            cin >> arr[i];
+            if(arr[i] == i+1)
+                count++;
+        }
+        if(count == 0)
+            cout << 0 << endl;
+        else if(count % 2 ==0)
+            cout  << count/2 << endl;
+        else
+            cout << count/2 + 1 << endl;
+
+
+    }
+    return 0;
+}
