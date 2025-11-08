269cabe7c1f8864f4270871c77dae2febc395dfa
Fahim <fahimimam786@gmail.com>
2025-11-08 23:36:31 +0600

    Another solution added

---
+++ CodeForces/Game Of Divisions.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        bool possible = false;
+        int index = -1;
+        for (int i = 0; i < n; i++) {
+            bool win = true;
+            for (int j = 0; j < n; j++) {
+                if (i != j && abs(a[i] - a[j]) % k == 0) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                possible = true;
+                index = i + 1;
+                break;
+            }
+        }
+        if (possible) {
+            cout << "YES\n" << index << endl;
+        } else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
