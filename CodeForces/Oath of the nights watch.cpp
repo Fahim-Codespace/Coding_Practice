d7299581297aed08673980ee1c24a61403e3cf8e
Fahim <fahimimam786@gmail.com>
2026-01-10 20:18:26 +0600

    Another Solution

---
+++ CodeForces/Oath of the nights watch.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+
+    int min = arr[0];
+    int max = arr[0];
+
+
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    int count = 0;
+    for(int i = 0; i < n; i++ ){
+
+        if(arr[i] > min && arr[i] < max){
+            count ++;
+        }
+    }
+
+    cout << count << endl;
+
+}
